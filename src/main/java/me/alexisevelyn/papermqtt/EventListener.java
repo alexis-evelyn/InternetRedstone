4a63b4249a8f5a7753df5ae56986ea79b3de7957
Alex Evelyn <gpg@senorcontento.com>
2020-03-26 00:45:59 -0400

    Still Not Even Close

---
+++ src/main/java/me/alexisevelyn/papermqtt/EventListener.java
@@ -0,0 +1,90 @@
+package me.alexisevelyn.papermqtt;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.Arrays;
+
+public class EventListener implements Listener {
+    Main main;
+
+    public EventListener(Main passed) {
+        this.main = passed;
+    }
+
+    @EventHandler
+    public void onRedstoneUpdate(BlockRedstoneEvent event) {
+        BlockState snapshot = event.getBlock().getState();
+
+        // TODO: Get Redstone Power Level Update For Signs
+        if (isSign(snapshot.getBlock().getType())) {
+            Sign sign = (Sign) snapshot.getBlock();
+
+            sign.setLine(1, ChatColor.DARK_GREEN + "RPU: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
+        }
+    }
+
+    @EventHandler
+    public void onSignChange(SignChangeEvent event) {
+        BlockState snapshot = event.getBlock().getState();
+
+        event.getPlayer().sendMessage(ChatColor.GOLD + "Redstone Power: " + ChatColor.DARK_GREEN + snapshot.getBlock().getBlockPower());
+
+        event.setLine(1, ChatColor.DARK_GREEN + "RP: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
+    }
+
+    @EventHandler
+    public void onSignClick(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
+            return;
+        if (event.getClickedBlock() == null)
+            return;
+
+        BlockState snapshot = event.getClickedBlock().getState();
+
+        if (isSign(snapshot.getBlock().getType())) {
+            // TODO: CraftBlock Cannot Be Cast To Sign
+            Sign sign;
+            try {
+//                sign = (Sign) snapshot.getBlock();
+                sign = (Sign) event.getClickedBlock();
+
+                sign.setLine(1, ChatColor.DARK_GREEN + "RPRC: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
+            } catch (ClassCastException exception) {
+                event.getPlayer().sendMessage(ChatColor.DARK_GREEN + "ClassCastException: " + ChatColor.DARK_RED + exception.getMessage());
+
+                this.main.getLogger().severe(ChatColor.GOLD + "ClassCastException: " + ChatColor.DARK_RED + Arrays.toString(exception.getStackTrace()));
+            }
+        }
+    }
+
+    public boolean isSign(Material object) {
+        // TODO: Read Sign Types From Config
+        // TODO: Relocate Function to Another Class (A General Purpose Sign Handling Class For Parsing/Handling Sign Data)
+
+        if (object == Material.ACACIA_SIGN || object == Material.ACACIA_WALL_SIGN) {
+            return true;
+        } else if (object == Material.BIRCH_SIGN || object == Material.BIRCH_WALL_SIGN) {
+            return true;
+        } else if (object == Material.DARK_OAK_SIGN || object == Material.DARK_OAK_WALL_SIGN) {
+            return true;
+        } else if (object == Material.JUNGLE_SIGN || object == Material.JUNGLE_WALL_SIGN) {
+            return true;
+        } else if (object == Material.OAK_SIGN || object == Material.OAK_WALL_SIGN) {
+            return true;
+        } else //noinspection RedundantIfStatement
+            if (object == Material.SPRUCE_SIGN || object == Material.SPRUCE_WALL_SIGN) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/me/alexisevelyn/papermqtt/Main.java
@@ -0,0 +1,17 @@
+package me.alexisevelyn.papermqtt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new EventListener(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
