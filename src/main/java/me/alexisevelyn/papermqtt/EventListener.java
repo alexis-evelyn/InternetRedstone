fa1991d4114e954a5293faea6784da554a391456
Alex Evelyn <gpg@senorcontento.com>
2020-03-26 02:15:31 -0400

    Closer...

--- src/main/java/me/alexisevelyn/papermqtt/EventListener.java
+++ src/main/java/me/alexisevelyn/papermqtt/EventListener.java
@@ -7,6 +7,7 @@ import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockRedstoneEvent;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -21,16 +22,34 @@ public class EventListener implements Listener {
     }
 
     @EventHandler
-    public void onRedstoneUpdate(BlockRedstoneEvent event) {
+    public void onRedstoneUpdate(BlockPhysicsEvent event) {
         BlockState snapshot = event.getBlock().getState();
 
         // TODO: Get Redstone Power Level Update For Signs
         if (isSign(snapshot.getBlock().getType())) {
-            Sign sign = (Sign) snapshot.getBlock();
+            Sign sign = (Sign) snapshot;
 
+            this.main.getLogger().info(ChatColor.DARK_GREEN + "RPU: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
             sign.setLine(1, ChatColor.DARK_GREEN + "RPU: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
+            sign.update();
         }
     }
+
+//    @EventHandler
+//    public void onRedstoneUpdate(BlockRedstoneEvent event) {
+//        BlockState snapshot = event.getBlock().getState();
+//
+//        // TODO: Get Redstone Power Level Update For Signs
+//        if (isSign(snapshot.getBlock().getType())) {
+//            Sign sign = (Sign) snapshot.getBlock();
+//
+//            this.main.getLogger().info(ChatColor.DARK_GREEN + "RPU: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
+//            sign.setLine(1, ChatColor.DARK_GREEN + "RPU: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
+//            sign.update();
+//        } else {
+//            this.main.getLogger().info(ChatColor.DARK_GREEN + "Redstone Update Block: " + ChatColor.DARK_RED + snapshot.getBlock().getType());
+//        }
+//    }
 
     @EventHandler
     public void onSignChange(SignChangeEvent event) {
@@ -45,6 +64,8 @@ public class EventListener implements Listener {
     public void onSignClick(PlayerInteractEvent event) {
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
             return;
+        if (!event.hasBlock())
+            return;
         if (event.getClickedBlock() == null)
             return;
 
@@ -55,13 +76,40 @@ public class EventListener implements Listener {
             Sign sign;
             try {
 //                sign = (Sign) snapshot.getBlock();
-                sign = (Sign) event.getClickedBlock();
+//                sign = (Sign) event.getClickedBlock();
+                sign = (Sign) snapshot;
+
+                event.getPlayer().sendMessage(ChatColor.DARK_GREEN + "RPRC: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
 
                 sign.setLine(1, ChatColor.DARK_GREEN + "RPRC: " + ChatColor.DARK_RED + snapshot.getBlock().getBlockPower());
+                sign.update(); // Attempts To Save Modified Sign Data From Snapshot - Fails If Sign Is No Longer In Same State As When Captured
             } catch (ClassCastException exception) {
                 event.getPlayer().sendMessage(ChatColor.DARK_GREEN + "ClassCastException: " + ChatColor.DARK_RED + exception.getMessage());
 
-                this.main.getLogger().severe(ChatColor.GOLD + "ClassCastException: " + ChatColor.DARK_RED + Arrays.toString(exception.getStackTrace()));
+                this.main.getLogger().severe(ChatColor.GOLD + "" + ChatColor.BOLD + "ClassCastException: "
+                            + ChatColor.DARK_RED + "" + ChatColor.BOLD + exception.getMessage());
+
+                this.main.getLogger().severe(ChatColor.GOLD + "" + ChatColor.BOLD + "Clicked Block: "
+                        + ChatColor.DARK_RED + "" + ChatColor.BOLD + snapshot.getType().toString());
+
+                this.main.getLogger().severe(ChatColor.GOLD + "" + ChatColor.BOLD + "Location: "
+                        + ChatColor.DARK_GREEN + "" + ChatColor.BOLD + "("
+                        + ChatColor.DARK_RED + "" + ChatColor.BOLD + snapshot.getLocation().getBlockX()
+                        + ChatColor.DARK_GREEN + "" + ChatColor.BOLD + ", "
+                        + ChatColor.DARK_RED + "" + ChatColor.BOLD + snapshot.getLocation().getBlockY()
+                        + ChatColor.DARK_GREEN + "" + ChatColor.BOLD + ", "
+                        + ChatColor.DARK_RED + "" + ChatColor.BOLD + snapshot.getLocation().getBlockZ()
+                        + ChatColor.DARK_GREEN + "" + ChatColor.BOLD + ")");
+
+                this.main.getLogger().severe(ChatColor.RED + "" + ChatColor.BOLD + "---");
+
+                for (StackTraceElement line : exception.getStackTrace()) {
+                    this.main.getLogger().severe(ChatColor.DARK_GREEN + "" + ChatColor.BOLD + line.getLineNumber()
+                            + ChatColor.RED + "" + ChatColor.BOLD + " - "
+                            + ChatColor.DARK_RED + "" + ChatColor.BOLD + line.toString());
+                }
+
+                this.main.getLogger().severe(ChatColor.RED + "" + ChatColor.BOLD + "---");
             }
         }
     }
